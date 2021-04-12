294a382bf3d8e455b6006ece386206c0ef93c926
darkcheftar <mtarunreddy111@gmail.com>
2021-04-12 22:43:00 +0530

    data-api-contact

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
+// Import the Joplin API
 import joplin from 'api';
-
 joplin.plugins.register({
+
 	onStart: async function() {
-		console.info('Test starteds!');
-	},
+		
+	const resources = await joplin.data.get(["resources"]);
+	console.log(resources);
+	const image = await joplin.data.get(["resources", resources[0].id,"file"], { fields: ['id','title','mime','created_time','updated_time','user_created_time','user_updated_time','file_extension','encryption_cipher_text','encryption_applied','encryption_blob_encrypted','size', 'is_shared',] });
+	console.log(image);
+	// tesseract.Tesseract
+    // .recognize(image.body)
+    // .progress(console.log)
+    // .then((res: any) => {
+    //     console.log(res);
+    // })
+    // .catch(console.error);
+	}
+
 });
+// 
